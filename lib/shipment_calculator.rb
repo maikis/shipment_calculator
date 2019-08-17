1f028277ee09b735e74877aad201db78675f748b
Mykolas Grinevicius <mykolas@grineviciai.eu>
2019-08-17 14:24:45 +0300

    Load providers from yaml config

--- lib/shipment_calculator.rb
+++ lib/shipment_calculator.rb
@@ -1,10 +1,20 @@
-require 'shipment_calculator/version'
+require 'yaml'
+require 'version'
+require 'provider'
 
 module ShipmentCalculator
   class Error < StandardError; end
 
-  # Main calculation "initializer". Accepts filename of input data file as a
+  # Main calculation "initializer". Accepts file name of input data file as a
   # parameter, defaults to 'input.txt' as stated in homework assignment.
-  def self.calculate(filename = 'input.txt')
+  def self.calculate(file_name = 'input.txt')
+    data_file = File.new("lib/shipment_calculator/data/#{file_name}", 'r')
+  end
+
+  def self.providers
+    provider_config = Psych.load_file('config/providers.yml')
+    provider_config.map do |short_name, sizes_with_prices|
+      Provider.new(short_name, sizes_with_prices)
+    end
   end
 end
--- spec/shipment_calculator_spec.rb
+++ spec/shipment_calculator_spec.rb
@@ -1,9 +1,17 @@
-RSpec.describe ShipmentCalculator do
-  it "has a version number" do
-    expect(ShipmentCalculator::VERSION).not_to be nil
-  end
+require 'spec_helper'
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe ShipmentCalculator do
+  describe '.providers' do
+    before do
+      allow(Psych).to receive(:load_file).with('config/providers.yml').and_return({ 'MR' => { 'S' => 1 } })
+    end
+
+    it 'returns array' do
+      expect(ShipmentCalculator.providers).to be_an(Array)
+    end
+
+    it 'returns Provider instances' do
+      expect(ShipmentCalculator.providers).to include(ShipmentCalculator::Provider)
+    end
   end
 end
