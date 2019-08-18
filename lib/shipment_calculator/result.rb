f80f7e742da2bb0969ac62bdd302a51380928281
Mykolas Grinevicius <mykolas@grineviciai.eu>
2019-08-18 18:12:37 +0300

    Update Result to allow modifying attributes

--- lib/shipment_calculator/result.rb
+++ lib/shipment_calculator/result.rb
@@ -1,8 +1,9 @@
 module ShipmentCalculator
   class Result
-    attr_reader :transaction, :shipment_price, :discount
+    attr_reader :transaction
+    attr_accessor :shipment_price, :discount
 
-    def initialize(transaction, shipment_price, discount)
+    def initialize(transaction, shipment_price = nil, discount = nil)
       @transaction = transaction
       @shipment_price = shipment_price
       @discount = discount
