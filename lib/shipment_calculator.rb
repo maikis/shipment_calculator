5b561f09eca4e0d4e81bc7a5a0470c026affde98
Mykolas Grinevicius <mykolas@grineviciai.eu>
2019-08-24 19:03:46 +0300

    Ugly version of fully working module

--- lib/shipment_calculator.rb
+++ lib/shipment_calculator.rb
@@ -5,6 +5,8 @@ require 'version'
 require 'calculator'
 require 'provider'
 require 'transaction'
+require 'result'
+require 'stdout_formatter'
 
 Dir[File.join(__dir__, 'shipment_calculator', 'rules', '*.rb')].each do |file|
   require file
@@ -20,6 +22,12 @@ module ShipmentCalculator
   # Main calculation "initializer". Accepts file name of input data file as a
   # parameter.
   def self.calculate(file_name)
+    tr_data = transaction_data(file_name)
+    tr_data_valid = tr_data.select(&:valid?)
+    [Rules::SmallShipmentLowestPriceRule, Rules::ThirdLargeFreeRule, Rules::BudgetRule].each do |rule|
+      rule.new(tr_data_valid).apply
+    end
+    Result.new(tr_data, StdoutFormatter.new).output_result
   end
 
   def self.transaction_data(file_name)
@@ -27,8 +35,14 @@ module ShipmentCalculator
     file_name = file_name.nil? ? 'input.txt' : file_name
     File.new("data/#{file_name}", 'r').map do |line|
       data = line.chomp.split(' ')
-      data[DATE] = Date.parse(data[DATE]) rescue nil
-      ShipmentCalculator::Transaction.new(data[DATE], data[SIZE], data[PRICE])
+      data[DATE] = Date.parse(data[DATE]) rescue data[DATE]
+      transaction = ShipmentCalculator::Transaction.new(data[DATE], data[SIZE], data[PRICE])
+      provider = providers.detect { |provider| provider.short_name == transaction.short_name }
+      if transaction.valid?
+        transaction.shipment_price = provider.price_by_size(transaction.size)
+        transaction.discount = 0
+      end
+      transaction
     end
   end
 
