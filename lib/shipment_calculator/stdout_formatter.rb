c5dce1f3c89ded72bb60ff3f4d35e6f76fadc57f
Mykolas Grinevicius <mykolas@grineviciai.eu>
2019-08-24 19:16:49 +0300

    Cleanup StdoutFormatter

--- lib/shipment_calculator/stdout_formatter.rb
+++ lib/shipment_calculator/stdout_formatter.rb
@@ -1,15 +1,15 @@
 module ShipmentCalculator
+  # Formatter class which prints to STDOUT. If module would have a need to
+  # evolve, this file could be moved to separate formatter folder and other
+  # formatters, like XML, file output, etc. might be added. But for now I
+  # decided to leave it in the main folder as we have no other formatter.
   class StdoutFormatter
     def output_result(context)
       context.transactions.each do |transaction|
         if transaction.valid?
-          puts "#{transaction.date} #{transaction.size} "\
-            "#{transaction.short_name} #{currency_format(transaction.shipment_price)} "\
-            "#{discount_format(transaction.discount)}"
+          puts all_values(transaction).join(' ')
         else
-          transaction_details = "#{transaction.date} #{transaction.size} "\
-            "#{transaction.short_name}".chomp
-          puts transaction_details + 'Ignored'
+          puts transaction_details(transaction).join(' ') + 'Ignored'
         end
       end
     end
@@ -22,6 +22,21 @@ module ShipmentCalculator
 
     def discount_format(amount)
       amount.positive? ? currency_format(amount) : '-'
+    end
+
+    def all_values(transaction)
+      transaction_details(transaction) + pricing_details(transaction)
+    end
+
+    def transaction_details(transaction)
+      [transaction.date,
+       transaction.size,
+       transaction.short_name]
+    end
+
+    def pricing_details(transaction)
+      [currency_format(transaction.shipment_price),
+       discount_format(transaction.discount)]
     end
   end
 end
